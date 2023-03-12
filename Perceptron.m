3ca5c41591661bc1fd5e38d10def4df3351e55f8
ardaeylen <80364562+ardaeylen@users.noreply.github.com>
2023-03-13 01:52:37 +0300

    Softmax, Categorical-CrossEntropy and flatten functions added.

--- Perceptron.m
+++ Perceptron.m
@@ -1,6 +1,127 @@
-input = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 30, 20, 50];
-labels = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25,61, 41, 101];
-model = letItLearn(input, labels, [1,3,1], 10000, 0.001, 'tanh','linear');
+%input = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 30, 20, 50];
+zero = [0, 1, 1, 1, 0;
+        1, 0, 0, 0, 1;
+        1, 0, 0, 0, 1;
+        1, 0, 0, 0, 1;
+        1, 0, 0, 0, 1;
+        1, 0, 0, 0, 1;
+        0, 1, 1, 1, 0];
+
+one = [0, 0, 1, 0, 0;
+       0, 1, 1, 0, 0;
+       1, 0, 1, 0, 0;
+       0, 0, 1, 0, 0;
+       0, 0, 1, 0, 0;
+       0, 0, 1, 0, 0;
+       1, 1, 1, 1, 1];
+
+two = [0, 1, 1, 1, 0;
+       1, 0, 0, 1, 0;
+       0, 0, 0, 1, 0;
+       0, 0, 0, 1, 0;
+       0, 0, 1, 0, 0;
+       0, 1, 0, 0, 0;
+       1, 1, 1, 1, 1];
+
+three = [0, 1, 1, 1, 1;
+         1, 0, 0, 0, 1;
+         0, 0, 0, 0, 1;
+         0, 1, 1, 1, 0;
+         0, 0, 0, 0, 1;
+         1, 0, 0, 0, 1;
+         0, 1, 1, 1, 1];
+
+four = [0, 0, 0, 0, 1;
+        0, 0, 0, 1, 0;
+        0, 0, 1, 0, 0;
+        0, 1, 0, 1, 0;
+        1, 1, 1, 1, 1;
+        0, 0, 0, 1, 0;
+        0, 0, 0, 1, 0];
+
+five = [0, 1, 1, 1, 1;
+        1, 0, 0, 0, 0;
+        1, 0, 0, 0, 0;
+        1, 1, 1, 1, 1;
+        0, 0, 0, 0, 1;
+        0, 0, 0, 0, 1;
+        1, 1, 1, 1, 1];
+
+six = [0, 1, 1, 1, 1;
+       1, 0, 0, 0, 0;
+       1, 0, 0, 0, 0;
+       1, 1, 1, 1, 0;
+       1, 0, 0, 0, 1;
+       1, 0, 0, 0, 1;
+       0, 1, 1, 1, 0];
+
+seven = [1, 1, 1, 1, 1;
+         0, 0, 0, 1, 0;
+         0, 0, 1, 0, 0;
+         0, 1, 1, 1, 0;
+         0, 0, 1, 0, 0;
+         0, 0, 1, 0, 0;
+         0, 0, 1, 0, 0];
+
+eight = [0, 1, 1, 1, 0;
+         1, 0, 0, 0, 1;
+         1, 0, 0, 0, 1;
+         0, 1, 1, 1, 0;
+         1, 0, 0, 0, 1;
+         1, 0, 0, 0, 1;
+         0, 1, 1, 1, 0];
+
+
+nine = [0, 1, 1, 1, 0;
+        1, 0, 0, 0, 1;
+        1, 0, 0, 0, 1;
+        0, 1, 1, 1, 1;
+        0, 0, 0, 0, 1;
+        0, 0, 0, 0, 1;
+        0, 1, 1, 1, 0];
+input = concat_input([zero; one; two; three; four; five; six; seven; eight; nine]);
+input = flatten_input(input);
+%labels = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25,61, 41, 101];
+labels = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0;
+          0, 1, 0, 0, 0, 0, 0, 0, 0, 0;
+          0, 0, 1, 0, 0, 0, 0, 0, 0, 0;
+          0, 0, 0, 1, 0, 0, 0, 0, 0, 0;
+          0, 0, 0, 0, 1, 0, 0, 0, 0, 0;
+          0, 0, 0, 0, 0, 1, 0, 0, 0, 0;
+          0, 0, 0, 0, 0, 0, 1, 0, 0, 0;
+          0, 0, 0, 0, 0, 0, 0, 1, 0, 0;
+          0, 0, 0, 0, 0, 0, 0, 0, 1, 0;
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 1];
+%model = letItLearn(input, labels, [10], 10000, 0.001, 'linear','softmax');
+
+% It is okey
+function [flattened_vector] = flattenFunction(matrix)
+flattened_vector = [];    
+for i = 1: size(matrix, 1)
+        row = matrix(i, :);
+        flattened_vector = [flattened_vector, row];
+end
+flattened_vector = flattened_vector.';
+end
+
+function [concatenated_input] = concat_input(input)
+k= 1;
+j =7;
+concatenated_input = zeros(size(input, 1)/10, size(input, 2), 10);
+for i=1:size(concatenated_input,3) % For all training examples:
+concatenated_input(:,:,i) =  input(k:j,:);
+k = k + 7;
+j = j + 7;
+end
+end
+
+
+function [flattened_input] = flatten_input(concatenated_input)
+flattened_input = zeros(size(concatenated_input,1) * size(concatenated_input, 2), size(concatenated_input, 3));
+for i=1:size(concatenated_input,3)
+flattened_input(:,i) = flattenFunction(concatenated_input(:, :, i));
+end
+end
 
 % It is okey
 function [row_sum_of_matrix] = row_sum(matrix)
@@ -12,6 +133,17 @@ function [row_sum_of_matrix] = row_sum(matrix)
     end
     row_sum_of_matrix = sum_matrix;
 end
+
+function [col_sum_of_matrix] =col_sum(matrix)
+    sum_matrix = zeros(1, size(matrix,2));
+    for i = 1:size(matrix,2)
+        for j = 1: size(matrix, 1)
+            sum_matrix(1,i) = sum_matrix(1,i) + matrix(i, j);
+        end
+    end
+    col_sum_of_matrix = sum_matrix;
+end
+
 %It is okey
 function [derivative_of_g] = derivative_of_activations(z,function_name)
 if(strcmp(function_name, 'sigmoid'))
@@ -22,6 +154,9 @@ if(strcmp(function_name, 'tanh'))
 end
 if(strcmp(function_name,'linear'))
     derivative_of_g = rdivide(z,z);
+end
+if (strcmp(function_name, 'softmax'))
+    derivative_of_g = activation_function(z, function_name).*(1-activation_function(z, function_name));
 end
 end
 % It is okey
@@ -44,6 +179,9 @@ function [activation] =  activation_function(z, function_name)
                 z(i,j) = 0;
             end
         end
+    end
+    if(strcmp(function_name, 'softmax'))
+        activation = softmax_activation(z);
     end
     end
 end
@@ -110,11 +248,13 @@ end
 end
 
 function [loss] = calculateLoss(predicted, actual, lastLayerActivationFunction)
-if strcmp(lastLayerActivationFunction,'sigmoid')
-    loss = r_divide(row_sum(-(actual * log(predicted) + (1 - actual)*log(1 - predicted))), size(actual,2));
-elseif strcmp(lastLayerActivationFunction,'linear')
-    loss= rdivide(row_sum((actual - predicted).^2).^(0.5),size(actual,2));
-end    
+    if strcmp(lastLayerActivationFunction,'sigmoid')
+        loss = r_divide(row_sum(-(actual.* log(predicted) + (1 - actual)*log(1 - predicted))), size(actual,2));
+    elseif strcmp(lastLayerActivationFunction,'linear')
+        loss= rdivide(row_sum((actual - predicted).^2).^(0.5),size(actual,2));
+    elseif strcmp(lossFunction, 'categorical_crossentropy')
+        loss = r_divide(row_sum(col_sum(-(actual).*log(predicted))), size(actual, 2));
+    end    
     loss_string = sprintf('Loss = %.5f ',(loss));
     disp(loss_string);
 end
@@ -123,12 +263,33 @@ function [da] = gradientOfLossWithRespToLastLayersActivations(lastActivations, a
 if strcmp(lossFunction, 'binary_crossentropy')
     da = rdivide(actual_values, lastActivations) * (-1) + rdivide(1 - actual_values, 1 - lastActivations);% loss = -(y * log(a) + (1 - y) * log(1 - a)).
 elseif strcmp(lossFunction, 'mean_squared_error')
-    da =  -(2 /size(actual_values,2))*(actual_values - lastActivations); % loss = sum ([(a - y)^2]) / m where m isthe number of training examples
-    
+    da =  -(2 /size(actual_values,2))*(actual_values - lastActivations); % loss = sum ([(a - y)^2]) / m where m is the number of training examples
+elseif strcmp(lossFunction, 'categorical_crossentropy')
+    da = rdivide(actual_values, lastActivations) * (-1);% loss = -sum(ylog(a)).
+    %In the specific (and usual) case of Multi-Class classification 
+    % the labels are one-hot, so only the positive class
+    %keeps its term in the loss.
 end
 end
 
+
+    function [probabilities] = softmax_activation(z)
+probabilities = zeros(size(z));
+for i = 1:size(z,2)
+    outputs = z(:,i);
+    sum = 0;
+    for j = 1: size(outputs)
+    sum = sum + exp(outputs(j));
+    end
+    probabilities(:,i) = rdivide(exp(outputs),sum); 
+end   
+    
+end
+
+
+
 function [model] = letItLearn(input,labels,layers, epochs, learning_rate, layer_activations, last_layer_activation)
+
 [weights, biases] = initialize_network(layers,size(input,1));
 
 activations = cell(1, size(layers,2)+1);
@@ -144,7 +305,7 @@ for i = 1:epochs
         end    
     end
     [loss] = calculateLoss(activations{layer + 1}, labels, last_layer_activation);
-    [da] = gradientOfLossWithRespToLastLayersActivations(activations{layer +1 }, labels,'mean_squared_error');
+    [da] = gradientOfLossWithRespToLastLayersActivations(activations{layer +1 }, labels,'categorical_crossentropy');
     %disp(da);
     for layer = size(layers, 2):-1:1
         [da, weights{layer}, biases{layer}] = backward_propagation(da, weights{layer}, biases{layer},activations{layer},weighted_inputs{layer}, learning_rate, layer_activations);
