b686de27d0cb20d9a1f9cae7005df877daacd2fa
ardaeylen <80364562+ardaeylen@users.noreply.github.com>
2023-05-11 01:27:28 +0300

    Confusion Matrix and model performance evaluation metrics added.

--- HANDWRITTEN_DIGIT_RECOGNITION/HandWrittenDigitRecognition_Perceptron.m
+++ HANDWRITTEN_DIGIT_RECOGNITION/HandWrittenDigitRecognition_Perceptron.m
@@ -16,7 +16,7 @@ classes = [0; 1; 2; 3; 4; 5; 6; 7; 8; 9];
 y_categorical = categorical(y_train, classes);
 %---------- Options-----------------
 options = trainingOptions('adam', ... % Sthocastic Gradient Descent Learning Algorithm Used
-    'MaxEpochs',200,...
+    'MaxEpochs',128,...
     'InitialLearnRate',1e-3, ... % Initial Learning Rate = 0,001
     'Verbose',true, ...
     'Plots','training-progress');
@@ -46,7 +46,64 @@ y_predicted = classify(net, transpose(X_test));
 final_accuracy = sum(y_predicted == categorical(transpose(y_test)),'all') / size(y_test,2); 
 %---------------------------------------------------
 
-conf_matrix = confusion(categorical(transpose(y_test)), y_predicted);
+conf_matrix = plotconfusion(categorical(transpose(y_test)), y_predicted);
+conf_matrix = confusionmat(categorical(transpose(y_test)), y_predicted);
+
+scores = statsOfMeasure(conf_matrix);
+disp(scores);
+
+
+function [stats] = statsOfMeasure(confusion)
+
+tp = [];
+fp = [];
+fn = [];
+tn = [];
+len = size(confusion, 1);
+for k = 1:len                  %  predict
+    % True positives           % | x o o |
+    tp_value = confusion(k,k); % | o o o | true
+    tp = [tp, tp_value];       % | o o o |
+                                               %  predict
+    % False positives                          % | o o o |
+    fp_value = sum(confusion(:,k)) - tp_value; % | x o o | true
+    fp = [fp, fp_value];                       % | x o o |
+                                               %  predict
+    % False negatives                          % | o x x |
+    fn_value = sum(confusion(k,:)) - tp_value; % | o o o | true
+    fn = [fn, fn_value];                       % | o o o |
+                                                                       %  predict
+    % True negatives (all the rest)                                    % | o o o |
+    tn_value = sum(sum(confusion)) - (tp_value + fp_value + fn_value); % | o x x | true
+    tn = [tn, tn_value];                                               % | o x x |
+end
+% Statistics of interest for confusion matrix
+prec = tp ./ (tp + fp); % precision
+sens = tp ./ (tp + fn); % sensitivity, recall
+spec = tn ./ (tn + fp); % specificity
+acc = sum(tp) ./ sum(sum(confusion));
+f1 = (2 .* prec .* sens) ./ (prec + sens);
+% For micro-average
+microprec = sum(tp) ./ (sum(tp) + sum(fp)); % precision
+microsens = sum(tp) ./ (sum(tp) + sum(fn)); % sensitivity, recall
+microspec = sum(tn) ./ (sum(tn) + sum(fp)); % specificity
+microacc = acc;
+microf1 = (2 .* microprec .* microsens) ./ (microprec + microsens);
+% Names of the rows
+name = ["true_positive"; "false_positive"; "false_negative"; "true_negative"; ...
+    "precision"; "sensitivity"; "specificity"; "accuracy"; "F-measure"];
+% Names of the columns
+varNames = ["name"; "classes"; "macroAVG"; "microAVG"];
+% Values of the columns for each class
+values = [tp; fp; fn; tn; prec; sens; spec; repmat(acc, 1, len); f1];
+% Macro-average
+macroAVG = mean(values, 2);
+% Micro-average
+microAVG = [macroAVG(1:4); microprec; microsens; microspec; microacc; microf1];
+% OUTPUT: final table
+stats = table(name, values, macroAVG, microAVG, 'VariableNames',varNames);
+
+end
 
 
 
